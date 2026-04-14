df38e22b56517c3443afe96247e0e9fc02f06fb1
The Lich <bavut08@hotmail.com>
2026-04-14 22:50:10 +0300

    Proje oluşturuldu
    Spring Initializer kullanılarak projeyi oluşturdum. Dependencyler : Spring Web, Spring Data JPA, PostgreSQL Driver, Validation, Spring Security ve Lombok.

---
+++ src/test/java/com/alpr/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
